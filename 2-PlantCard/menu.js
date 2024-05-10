4613fd520f2d8347d8bb27147bf69c8c4ef44abe
Mikelly Correia <163700231+MiCorreia@users.noreply.github.com>
2024-05-10 00:53:21 -0300

    Arquivo javascript do menu

---
+++ 2-PlantCard/menu.js
@@ -0,0 +1,13 @@
+function menuRedes(){
+    let menuObjeto = document.getElementById('menu-objeto');
+    
+    if (menuObjeto.style.visibility=== 'hidden') {
+    
+        menuObjeto.style.visibility = 'visible';
+    } 
+
+    else{
+        menuObjeto.style.visibility = 'hidden';
+        
+    }
+}
